466fd102386eb2fd79ba18a960cd88e24b3bec9d
Frank Göldner <frank.goeldner@myhotelshop.de>
2020-05-27 18:44:30 +0200

    simplified xml annotations

--- pkg/gha/hint.go
+++ pkg/gha/hint.go
@@ -8,58 +8,42 @@ import (
 // Defines a Hint Request message that contains the time Google last
 // received an update from your server.
 type HintRequest struct {
-	XMLName       xml.Name  `xml:"HintRequest"`
+	XMLName       xml.Name  `xml:""`
 	ID            string    `xml:"id,attr"`
 	Timestamp     time.Time `xml:"timestamp,attr"`
-	LastFetchTime time.Time `xml:"LastFetchTime"`
+	LastFetchTime time.Time `xml:""`
 }
 
 // Hint Response message that specifies the hotels whose prices have
 // changed since the last time Google received a successful Hint Response
 // from those same servers.
 type Hint struct {
-	XMLName xml.Name `xml:"Hint"`
-	Item    []Item   `xml:"Item"`
+	XMLName xml.Name `xml:""`
+	Item    []Item   `xml:""`
 }
 
 // A container for the hotel/itinerary to be updated.
 type Item struct {
-	XMLName             xml.Name             `xml:"Item"`
-	Property            []Property           `xml:"Property"`
-	FirstDate           string               `xml:"FirstDate,omitempty"`
-	LastDate            string               `xml:"LastDate,omitempty"`
-	Stay                *Stay                `xml:"Stay,omitempty"`
-	StaysIncludingRange *StaysIncludingRange `xml:"StaysIncludingRange,omitempty"`
-}
-
-// The ID of a hotel, using the same ID as the Hotel List Feed. The number of
-// <Property> elements you can specify in a single <Item> block is determined
-// by the type of Hint Response message:
-// - Exact itineraries:
-//   Up to 100 hotels.
-// - Check-in ranges:
-//   More than one if you set <MultipleItineraries> to "checkin_range" in your
-//   <QueryControl> message.
-// - Ranged stay:
-//   More than one if you set <MultipleItineraries> to "affected_dates"
-//   in your <QueryControl> message.
-type Property struct {
-	XMLName xml.Name `xml:"Property"`
-	ID      string   `xml:",chardata"`
+	XMLName             xml.Name             `xml:""`
+	Property            []Property           `xml:""`
+	FirstDate           string               `xml:",omitempty"`
+	LastDate            string               `xml:",omitempty"`
+	Stay                *Stay                `xml:",omitempty"`
+	StaysIncludingRange *StaysIncludingRange `xml:",omitempty"`
 }
 
 // A container for the checkin date and length of stay elements in  an exact
 // itinerary Hint Response message. Each <Item> can contain only a single <Stay>.
 type Stay struct {
-	XMLName      xml.Name `xml:"Stay"`
-	CheckInDate  string   `xml:"CheckInDate"`
-	LengthOfStay int8     `xml:"LengthOfStay"`
+	XMLName      xml.Name `xml:""`
+	CheckInDate  string   `xml:""`
+	LengthOfStay int8     `xml:""`
 }
 
 // A container for the first date and last date elements in a ranged stay
 // Hint Response message.
 type StaysIncludingRange struct {
-	XMLName   xml.Name `xml:"StaysIncludingRange"`
-	FirstDate string   `xml:"FirstDate"`
-	LastDate  string   `xml:"LastDate"`
+	XMLName   xml.Name `xml:""`
+	FirstDate string   `xml:""`
+	LastDate  string   `xml:""`
 }
