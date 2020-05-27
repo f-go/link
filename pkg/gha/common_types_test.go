d0d9822480290f7bcd55c0887254d154bbb653f0
Frank Göldner <frank.goeldner@myhotelshop.de>
2020-05-27 19:07:40 +0200

    added tests for common types

---
+++ pkg/gha/common_types_test.go
@@ -0,0 +1,38 @@
+package gha
+
+import (
+	"encoding/xml"
+	"testing"
+)
+
+func TestPropertyStruct(t *testing.T) {
+	example := "<Property>abc</Property>"
+
+	var p Property
+	if err := xml.Unmarshal([]byte(example), &p); err != nil {
+		t.Errorf("Unmarshal data failed. %v", err)
+	}
+
+	if p.XMLName.Local != "Property" {
+		t.Errorf("XMLName.Local got %v, want: Property", p.XMLName.Local)
+	}
+	if p.ID != "abc" {
+		t.Errorf("ID got %v, want: abc", p.ID)
+	}
+}
+
+func TestMoneyStruct(t *testing.T) {
+	example := "<Money currency=\"USD\">13.54</Money>"
+
+	var m Money
+	if err := xml.Unmarshal([]byte(example), &m); err != nil {
+		t.Errorf("Unmarshal data failed. %v", err)
+	}
+
+	if m.Value != 13.54 {
+		t.Errorf("Value got %v, want: 13.54", m.Value)
+	}
+	if m.Currency != "USD" {
+		t.Errorf("ID got %v, want: USD", m.Currency)
+	}
+}
